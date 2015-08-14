06995c67880969f8ed3fbfe563ed3dda30a3d728
CAndeMan <corey_anderson2012@hotmail.com>
2015-08-13 21:53:19 -0500

    Added missing dependecies.

---
+++ LolApp/LolApp/Models/ChampionListModel.cs
@@ -0,0 +1,22 @@
+﻿using LolApp.Common.ChampionInfo;
+using LolApp.Common.RiotAPI;
+using LolApp.Common.RiotAPI.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LolApp.Models
+{
+    public class ChampionListModel
+    {
+        public List<ChampionInfoDto> list;
+
+        public ChampionListModel()
+        {
+            var qry = new Champion();
+            list = RequestBuilder.ExecuteAndReturn(qry);
+        }
+
+    }
+}
