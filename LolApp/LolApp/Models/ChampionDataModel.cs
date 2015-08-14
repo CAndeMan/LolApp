702dda8f0f845d52617d277718ba8ddc7f2c1297
CAndeMan <corey_anderson2012@hotmail.com>
2015-08-14 18:09:50 -0500

    Model for Champ data

---
+++ LolApp/LolApp/Models/ChampionDataModel.cs
@@ -0,0 +1,23 @@
+﻿using LolApp.Common.RiotAPI;
+using LolApp.Common.RiotAPI.API;
+using LolApp.Common.StaticData.Champion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LolApp.Models
+{
+    public class ChampionDataModel
+    {
+        public ChampionDto champ;
+
+        public ChampionDataModel(int champId)
+        {
+            var qry = new StaticData<ChampionDto>(6, champId, null, null, "all");
+            var list = RequestBuilder.ExecuteAndReturn(qry);
+            champ = list.First();
+
+        }
+    }
+}
