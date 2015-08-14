d5def2d8d23cc89235449b75568b262580d687b0
CAndeMan <corey_anderson2012@hotmail.com>
2015-08-13 21:29:04 -0500

    Removed deprecated crap. Added test model for view.

--- LolApp/LolApp/Controllers/HomeController.cs
+++ LolApp/LolApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LolApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,9 +16,9 @@ namespace LolApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "The Champion List page.";
 
-            return View();
+            return View(new ChampionListModel());
         }
 
         public ActionResult Contact()
