43f67d7d8e4707a54307051a810c736de8e2f41a
Biosaw <isaacknet@yahoo.com>
2015-08-14 21:50:21 -0500

    Added champion controller and moved views

---
+++ LolApp/LolApp/Controllers/ChampionController.cs
@@ -0,0 +1,30 @@
+﻿using LolApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LolApp.ChampionController
+{
+    public class ChampionController : Controller
+    {
+        // GET: Champion
+        public ActionResult Index()
+        {
+            ViewBag.Message = "The Champion List page.";
+
+            return View(new ChampionListModel());
+        }
+    
+        public ActionResult Champion(int? id)
+        {
+            if (id == null) return Redirect("About");
+
+            ViewBag.Message = "A Champion's Profile";
+
+            return View(new ChampionDataModel(id ?? 0));
+        }
+
+    }
+}
--- LolApp/LolApp/Controllers/HomeController.cs
+++ LolApp/LolApp/Controllers/HomeController.cs
@@ -14,22 +14,7 @@ namespace LolApp.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "The Champion List page.";
-
-            return View(new ChampionListModel());
-        }
-
-        public ActionResult Champion(int? id)
-        {
-            if (id == null) return Redirect("About");
-
-            ViewBag.Message = "A Champion's Profile";
-
-            return View(new ChampionDataModel(id ?? 0));
-        }
-
+   
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
